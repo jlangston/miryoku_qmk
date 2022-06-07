0f8381ac486facd128192230bc598ce239f48d14
Jeff Langston <jeff@prsgrp.dev>
2022-06-07 11:09:33 -0600

    switching quote to semi

--- users/manna-harbour_miryoku/miryoku_babel/miryoku_layer_alternatives.h
+++ users/manna-harbour_miryoku/miryoku_babel/miryoku_layer_alternatives.h
@@ -118,9 +118,9 @@ U_NA,              U_NP,              U_NP,              LT(U_MEDIA,KC_ESC),LT(U
 
 #define MIRYOKU_ALTERNATIVES_BASE_QWERTY \
 KC_TILD,           KC_Q,              KC_W,              KC_E,              KC_R,              KC_T,              KC_Y,              KC_U,              KC_I,              KC_O,              KC_P,                    KC_BSLS, \
-KC_LCTL,           LGUI_T(KC_A),      LALT_T(KC_S),      LCTL_T(KC_D),      LSFT_T(KC_F),      KC_G,              KC_H,              LSFT_T(KC_J),      LCTL_T(KC_K),      LALT_T(KC_L),      LGUI_T(KC_QUOT),         KC_QUOT, \
+KC_LCTL,           LGUI_T(KC_A),      LALT_T(KC_S),      LCTL_T(KC_D),      LSFT_T(KC_F),      KC_G,              KC_H,              LSFT_T(KC_J),      LCTL_T(KC_K),      LALT_T(KC_L),      LGUI_T(KC_SCLN),         KC_QUOT, \
 KC_LSFT,           LT(U_BUTTON,KC_Z), ALGR_T(KC_X),      KC_C,              KC_V,              KC_B,              KC_N,              KC_M,              KC_COMM,           ALGR_T(KC_DOT),    LT(U_BUTTON,KC_SLSH),    KC_COLN, \
-U_NA,              U_NP,              U_NP,              LT(U_MEDIA,KC_ESC), LT(U_NAV,KC_SPC),  LT(U_MOUSE,KC_TAB),LT(U_SYM,KC_ENT),  LT(U_NUM,KC_BSPC), LT(U_FUN,KC_DEL),  U_NP,              U_NP,              U_NA                  
+U_NA,              U_NP,              U_NP,              LT(U_MEDIA,KC_ESC), LT(U_NAV,KC_SPC),  LT(U_MOUSE,KC_TAB),LT(U_SYM,KC_ENT),  LT(U_NUM,KC_BSPC), LT(U_FUN,KC_DEL),  U_NP,             U_NP,              U_NA                  
 
 #define MIRYOKU_ALTERNATIVES_BASE_QWERTZ \
 KC_TILD,           KC_Q,              KC_W,              KC_E,              KC_R,              KC_T,              KC_Z,              KC_U,              KC_I,              KC_O,              KC_P,                KC_BSLS, \
