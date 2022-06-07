24f7cbc487aa5b2da1d752f9c6cbdc8d1fb9f592
Jeff Langston <jeff@prsgrp.dev>
2022-06-07 01:01:07 -0600

    enable the rgb matrix

--- keyboards/crkbd/rules.mk
+++ keyboards/crkbd/rules.mk
@@ -19,7 +19,7 @@ NKRO_ENABLE = yes           # USB Nkey Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
-RGB_MATRIX_ENABLE = no
+RGB_MATRIX_ENABLE = yes
 RGB_MATRIX_DRIVER = WS2812
 
 # if firmware size over limit, try this option
